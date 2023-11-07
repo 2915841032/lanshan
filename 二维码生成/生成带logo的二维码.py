da642985ca2d4565f96dc003cb4c028d28c5c5bd
阿波 <111975372+2915841032@users.noreply.github.com>
2023-11-07 23:29:21 +0800

    Add files via upload

---
+++ 二维码生成/生成带logo的二维码.py
@@ -0,0 +1,27 @@
+﻿import qrcode
+from PIL import Image
+# 定义二维码内容
+data = '我爱你啊'
+
+# 生成二维码图像
+img = qrcode.make(data)
+
+# # 加载logo图像
+# logo = Image.open('1.jpg')
+#
+# # 计算logo的大小
+# w, h = img.size
+# logo_w = int(w / 4)
+# logo_h = int(h / 4)
+#
+# # 调整logo的大小
+# logo = logo.resize((logo_w, logo_h))
+#
+# # 计算logo的位置
+# pos = ((w - logo_w) // 2, (h - logo_h) // 2)
+#
+# # 合并二维码和logo
+# img.paste(logo, pos)
+
+# 保存二维码图像
+img.save('qrcode_logo1.png')
---
+++ 二维码生成/简单示例.py
@@ -0,0 +1,34 @@
+﻿import qrcode
+
+# 定义二维码内容
+data = 'https://www.baidu.com'
+
+# 生成二维码图像
+img = qrcode.make(data)
+
+# 保存二维码图像
+img.save('qrcode.png')
+
+
+
+
+from PIL import Image
+
+
+# 定义二维码内容
+data = 'https://www.baidu.com'
+
+# 生成二维码图像
+img = qrcode.make(data)
+
+# 加载背景图像
+bg = Image.open('1.jpg')
+
+# 调整背景图像大小
+bg = bg.resize(img.size)
+
+# 合并二维码和背景
+img_bg = Image.alpha_composite(bg.convert('RGBA'), img.convert('RGBA'))
+
+# 保存二维码图像
+img_bg.save('qrcode_bg.png')
